4249cc86da158eef41115fb84b93b6fa0b9214e7
Ronald van Kouwen <ronaldvk90@outlook.com>
2026-07-12 22:53:49 +0200

    Added home-manager for debbie

---
+++ home/debbie/home.nix
@@ -0,0 +1,28 @@
+{ config, pkgs, lib, ... }:
+
+  {
+  home.username = "debbie";
+  home.homeDirectory = "/home/debbie";
+  home.stateVersion = "26.05";
+  home.packages = with pkgs; [
+    steam
+    libreoffice
+    vlc
+    signal-desktop
+    google-chrome
+    whatsapp-electron
+    thunderbird
+    pulseaudio
+    teams-for-linux
+  ];
+
+  #home.sessionVariables = {
+  #};
+
+  home.file = {
+    ".config/pipewire/pipewire.conf.d/zeroconf-discover.conf".source = ./configs/pipewire/zeroconf-discover.conf;
+  };
+
+  #nixpkgs.config.allowUnfree = true;
+  programs.home-manager.enable = true;
+}
