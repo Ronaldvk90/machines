3981d33de3fea12873fde9dd796f92e41780ba87
Ronald van Kouwen <Ronaldvk90@outlook.com>
2026-07-15 14:32:24 +0200

    Confilcting IP. Changed temporarily

--- hosts/strickland/configuration.nix
+++ hosts/strickland/configuration.nix
@@ -13,7 +13,7 @@
     };
   };
   networking.interfaces.br0.ipv4.addresses = [ {
-    address = "10.10.10.2";
+    address = "10.10.10.222";
     prefixLength = 24;
   } ];
   networking.defaultGateway = "10.10.10.1";
