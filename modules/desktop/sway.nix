10f9da4dd009feeb63a39382a4843aeed2798dfb
Ronald van Kouwen <ronaldvk90@outlook.com>
2026-07-15 13:41:15 +0200

    Set greetd for login

--- modules/desktop/sway.nix
+++ modules/desktop/sway.nix
@@ -18,13 +18,16 @@
     };
   };
 
-  services.displayManager.sddm = {
-    enable = true;
-  
-    wayland = {
-      enable = true;
-      };
+  services.greetd = {                                                      
+  enable = true;                                                         
+  settings = {                                                           
+    default_session = {                                                  
+      command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd sway";
+      user = "greeter";                                                  
+      };                                                                   
+    };                                                                     
   };
+
 
   security.pam.services.swaylock = {};
   security.pam.loginLimits = [
