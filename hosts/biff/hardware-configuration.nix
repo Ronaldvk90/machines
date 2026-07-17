21b23935c1f4e04fb07081aea624b171cbea2407
Ronald van Kouwen <Ronaldvk90@outlook.com>
2026-07-17 13:18:35 +0200

    set correct disk id's

--- hosts/biff/hardware-configuration.nix
+++ hosts/biff/hardware-configuration.nix
@@ -6,12 +6,12 @@
     ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/b7b9ef9a-6aac-4dcd-b6d6-0f499eccbaeb";
+    { device = "/dev/disk/by-uuid/4dc154eb-9e30-4e0d-a225-57e3b2daaa19";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/546C-F1D1";
+    { device = "/dev/disk/by-uuid/E7F4-23E4";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
