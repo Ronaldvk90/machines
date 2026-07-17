b0451debb3e1a209dee8b6229cf445321c21cd11
Ronald van Kouwen <Ronaldvk90@outlook.com>
2026-07-17 13:11:25 +0200

    emergency patch

--- flake.nix
+++ flake.nix
@@ -84,7 +84,8 @@ outputs = { self, nixpkgs, home-manager, disko, ... }: {
         ./modules/services/zsh.nix
         ./modules/users/ronald.nix
         ./hosts/biff/configuration.nix
-        ./hosts/biff/disk.nix
+        ./hosts/biff/hardware-configuration.nix
+        #./hosts/biff/disk.nix
         home-manager.nixosModules.home-manager
         disko.nixosModules.disko
       ];
---
+++ hosts/biff/hardware-configuration.nix
@@ -0,0 +1,23 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/b7b9ef9a-6aac-4dcd-b6d6-0f499eccbaeb";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/546C-F1D1";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
+    };
+
+  swapDevices = [ ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
