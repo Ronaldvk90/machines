b9b385940079fba26e0627720621039b600cf956
Ronald van Kouwen <Ronaldvk90@outlook.com>
2026-07-17 21:53:05 +0200

    Added flake hostname

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-HOST="${1:?Target hostname/IP}"
+HOST="${1:?Target IP}"
+MACHINE="${2:?Machine name}"
 
 nix shell nixpkgs#nixos-anywhere -c \
   nixos-anywhere \
-    --flake github:ronaldvk90/machines#strickland \
+    --flake github:ronaldvk90/machines#$MACHINE \
     root@"$HOST"
