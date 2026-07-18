204d9415e034966b23013f1e2c7e470b92800425
Ronald van Kouwen <ronaldvk90@outlook.com>
2026-07-18 18:47:57 +0200

    disko test for biff

--- flake.nix
+++ flake.nix
@@ -95,8 +95,8 @@ nixosConfigurations.pxeinstaller =
         ./modules/services/zsh.nix
         ./modules/users/ronald.nix
         ./hosts/biff/configuration.nix
-        ./hosts/biff/hardware-configuration.nix
-        #./hosts/biff/disk.nix
+        #./hosts/biff/hardware-configuration.nix
+        ./hosts/biff/disk.nix
         home-manager.nixosModules.home-manager
         disko.nixosModules.disko
       ];
