6b1901787b56a06e9200ab119ebf9cab5c18f1e9
Ronald van Kouwen <Ronaldvk90@outlook.com>
2026-07-19 22:05:08 +0200

    Edited nixos anywhere script. Trial and errorwhise

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 HOST="${1:?Target IP}"
 MACHINE="${2:?Machine name}"
 
-nix shell nixpkgs#nixos-anywhere -c nixos-anywhere --flake github:ronaldvk90/machines#$MACHINE root@"$HOST"
+nix shell nixpkgs#nixos-anywhere -c nixos-anywhere --phases kexec,disko,install --flake github:ronaldvk90/machines#$MACHINE root@"$HOST"
