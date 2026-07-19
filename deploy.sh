d2ae1af0a2031ff87094b8860ede0fea1222fd3e
Ronald van Kouwen <Ronaldvk90@outlook.com>
2026-07-19 17:08:10 +0200

    Leftover code removal

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,6 @@ set -euo pipefail
 HOST="${1:?Target IP}"
 MACHINE="${2:?Machine name}"
 
-if [[ -z "$KEY" ]]; then
-    echo "No key supplied!"
-    exit 1
-fi
-
 nix shell nixpkgs#nixos-anywhere -c nixos-anywhere --flake github:ronaldvk90/machines#$MACHINE root@"$HOST"
 
 HOST=10.10.10.222
