0346a6f19045fde630347666bb047d11ff61443b
Ronald van Kouwen <Ronaldvk90@outlook.com>
2026-07-19 17:07:38 +0200

    Trying to automate my crap....

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,6 @@ set -euo pipefail
 HOST="${1:?Target IP}"
 MACHINE="${2:?Machine name}"
 
-read -r -s -p "Age private key: " KEY
-printf '\n'
-
 if [[ -z "$KEY" ]]; then
     echo "No key supplied!"
     exit 1
@@ -20,4 +17,8 @@ while ! ping -c 1 -n -w 1 $HOST &> /dev/null
 do
     printf "%c" "."
 done
-    printf '%s' "$KEY" | ssh ronald@"$HOST" 'sudo mkdir -p /var/lib/agenix && sudo install -m 600 /dev/stdin /var/lib/agenix/agenix.key'
+ echo "Paste age private key, then press CTRL+D:"
+ssh ronald@"$HOST" '
+  sudo install -d -m 700 /var/lib/agenix &&
+  sudo install -m 600 /dev/stdin /var/lib/agenix/agenix.key
+'
