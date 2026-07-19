79d3c619d91846f29d05eb98cc2656aaf861a2a7
Ronald van Kouwen <Ronaldvk90@outlook.com>
2026-07-19 22:34:16 +0200

    Trial and error.

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,5 @@ MACHINE="${2:?Machine name}"
 nix shell nixpkgs#nixos-anywhere -c nixos-anywhere --phases kexec,disko,install --flake github:ronaldvk90/machines#$MACHINE root@"$HOST"
 
 echo "Paste your age key, finish with CTRL-D:"
-ssh root@"$HOST" 'install -m 600 /dev/stdin /mnt/var/lib/agenix/agenix.key'
-ssh root@"$HOST" "cd /mnt && nixos-enter && nixos-rebuild switch --flake github:ronaldvk90/machines#$MACHINE"
+ssh root@"$HOST" 'mkdir -p /mnt/var/lib/agenix && install -m 600 /dev/stdin /mnt/var/lib/agenix/agenix.key'
+ssh root@"$HOST" "cd /mnt && nixos-enter --command nixos-rebuild boot --flake github:ronaldvk90/machines#$MACHINE"
