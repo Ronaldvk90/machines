93ed51a23e39cabce6e41e8fb4a7279be07d334b
Ronald van Kouwen <Ronaldvk90@outlook.com>
2026-07-19 18:37:20 +0200

    Modified cifs for agenix

--- modules/services/samba.nix
+++ modules/services/samba.nix
@@ -5,19 +5,19 @@
   fileSystems."/mnt/ronald" = {
     device = "//10.10.10.3/ronald";
     fsType = "cifs";
-    options = [ "credentials=/root/.ronald" "x-systemd.automount" "auto" "uid=1000" "gid=100" "file_mode=0700" "dir_mode=0700" ];
+    options = [ "credentials=/run/agenix/ronaldNasCredentials" "x-systemd.automount" "auto" "uid=1000" "gid=100" "file_mode=0700" "dir_mode=0700" ];
   };
 
   fileSystems."/mnt/debbie" = {
     device = "//10.10.10.3/debbie";
     fsType = "cifs";
-    options = [ "credentials=/root/.debbie" "x-systemd.automount" "auto" "uid=1000" "gid=100" "file_mode=0700" "dir_mode=0700" ];
+    options = [ "credentials=/run/agenix/debbieNasCredentials" "x-systemd.automount" "auto" "uid=1000" "gid=100" "file_mode=0700" "dir_mode=0700" ];
   };
 
 #  fileSystems."/mnt/ps2" = {
 #    device = "//10.10.10.3/ps2";
 #    fsType = "cifs";
-#    options = [ "credentials=/root/.ronald" "x-systemd.automount" "auto" "uid=1000" "gid=100" "file_mode=0700" "dir_mode=0700" ];
+#    options = [ "credentials=/run/agenix/ronaldNasCredentials" "x-systemd.automount" "auto" "uid=1000" "gid=100" "file_mode=0700" "dir_mode=0700" ];
 #  };
 
   environment.systemPackages = with pkgs; [
