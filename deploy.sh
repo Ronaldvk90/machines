e210af502d316470ccfdde03a8cd7d76cee8b85b
Ronald van Kouwen <Ronaldvk90@outlook.com>
2026-07-19 16:50:15 +0200

    Trying to automize installs

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,20 @@ set -euo pipefail
 HOST="${1:?Target IP}"
 MACHINE="${2:?Machine name}"
 
+read -r -s -p "Age private key: " KEY
+printf '\n'
+
+if [[ -z "$KEY" ]]; then
+    echo "No key supplied!"
+    exit 1
+fi
+
 nix shell nixpkgs#nixos-anywhere -c nixos-anywhere --flake github:ronaldvk90/machines#$MACHINE root@"$HOST"
+
+HOST=10.10.10.222
+printf "%s" "waiting for host to come up..."
+while ! ping -c 1 -n -w 1 $HOST &> /dev/null
+do
+    printf "%c" "."
+done
+    printf '%s' "$KEY" | ssh ronald@"$HOST" 'sudo mkdir -p /var/lib/agenix && sudo install -m 600 /dev/stdin /var/lib/agenix/agenix.key'
