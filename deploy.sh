19d515dfe3b877c21b539823b6237ed035dccb2b
Ronald van Kouwen <Ronaldvk90@outlook.com>
2026-07-19 22:16:56 +0200

    Trial and error with deployment

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,6 @@ HOST="${1:?Target IP}"
 MACHINE="${2:?Machine name}"
 
 nix shell nixpkgs#nixos-anywhere -c nixos-anywhere --phases kexec,disko,install --flake github:ronaldvk90/machines#$MACHINE root@"$HOST"
+
+echo "Paste your age key, finish with CTRL-D:"
+ssh root@"$HOST" 'sudo install -m 600 /dev/stdin /mnt/var/lib/agenix/agenix.key'
