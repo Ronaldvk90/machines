da4cbc59aa8dfc152ff78e9f1cd4c36d400e81d8
Ronald van Kouwen <Ronaldvk90@outlook.com>
2026-07-20 21:22:43 +0200

    fixing jellyfin permissions

--- modules/services/jellyfin.nix
+++ modules/services/jellyfin.nix
@@ -5,4 +5,15 @@
     enable = true;
     openFirewall = true;
   };
+
+  users.groups.media = {
+    gid = 2000;
+  };
+  users.users.ronald.extraGroups = [
+    "media"
+  ];
+
+  users.users.jellyfin.extraGroups = [
+    "media"
+  ];
 }
