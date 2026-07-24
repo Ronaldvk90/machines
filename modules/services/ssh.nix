34fd9bafe431b079e0a9f5d388158a44cee0f5fd
Ronald van Kouwen <Ronaldvk90@outlook.com>
2026-07-24 11:36:26 +0200

    Copyed ssh nix file for trial and error

--- modules/services/ssh.nix
+++ modules/services/ssh.nix
@@ -1,47 +1,23 @@
 { config, pkgs, lib, ... }:
 
 {
-  security.pam.services.sshd = {
-    googleAuthenticator.enable = true;
-
-    rules.auth = {
-      google_authenticator = {
-        order = 11000;
-        control = "required";
-        modulePath =
-          "${pkgs.google-authenticator}/lib/security/pam_google_authenticator.so";
-      };
-    };
-  };
-
-  services.openssh = {
-    enable = true;
-    openFirewall = true;
-
-    settings = {
-      PasswordAuthentication = false;
-      KbdInteractiveAuthentication = true;
-      PubkeyAuthentication = true;
-      UsePAM = true;
-
-      AuthenticationMethods = "publickey,keyboard-interactive";
-
-      PermitRootLogin = "no";
-      AllowUsers = [ "ronald" ];
-
-      X11Forwarding = false;
-      AllowTcpForwarding = false;
-      AllowAgentForwarding = false;
-
-      LoginGraceTime = "20s";
-      MaxAuthTries = 3;
-
-      PerSourcePenalties =
-        "crash:3600s authfail:3600s max:86400s";
-    };
-  };
-
-  environment.systemPackages = with pkgs; [
+  packages = with pkgs; [
     google-authenticator
   ];
+  
+  services = {
+    openssh = {
+      enable = true;
+      settings ={
+        PasswordAuthentication = true;
+        PermitRootLogin = "no";
+        KbdInteractiveAuthentication = true;
+        ChallengeResponseAuthentication = yes;
+        };
+      };
+  };
+  
+  security.pam = {
+    services.sshd.googleAuthenticator.enable = true;
+  };
 }
