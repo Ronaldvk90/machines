13ace1f0df827eba9caf31cc69699e99152de242
Ronald van Kouwen <Ronaldvk90@outlook.com>
2026-07-24 11:55:59 +0200

    trial and error

--- modules/services/ssh.nix
+++ modules/services/ssh.nix
@@ -12,20 +12,15 @@
           PasswordAuthentication = false;
           KbdInteractiveAuthentication = true;
           PubkeyAuthentication = true;
-          UsePAM = true;
-
-          AuthenticationMethods = "publickey,keyboard-interactive";
-
+          #UsePAM = true;
+          #AuthenticationMethods = "publickey,keyboard-interactive";
           PermitRootLogin = "no";
           AllowUsers = [ "ronald" ];
-
           X11Forwarding = false;
           AllowTcpForwarding = false;
           AllowAgentForwarding = false;
-
           LoginGraceTime = "20s";
           MaxAuthTries = 3;
-
           PerSourcePenalties =
           "crash:3600s authfail:3600s max:86400s";
         };
