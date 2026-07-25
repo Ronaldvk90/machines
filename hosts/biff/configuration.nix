90ff0272fbb97a39b8ba3840a6d14e94cf756bfd
Ronald van Kouwen <Ronaldvk90@outlook.com>
2026-07-25 21:20:58 +0200

    Made AgeNix Wifi and OpenVPN credentials machine dependend

--- hosts/biff/configuration.nix
+++ hosts/biff/configuration.nix
@@ -8,9 +8,20 @@
 
   networking.hostName = "biff"; # Define your hostname.
   networking.networkmanager.enable = true;
+
+  ##### Wifi Networks #####
   age.secrets.Ziggo-ap-4d4efe6 = {
-   file = ../../secrets/Ziggo-ap-4d4efe6.nmconnection.age;
-   path = "/etc/NetworkManager/system-connections/Ziggo-ap-4d4efe6.nmconnection";
+    file = ../../secrets/Ziggo-ap-4d4efe6.nmconnection.age;
+    path = "/etc/NetworkManager/system-connections/Ziggo-ap-4d4efe6.nmconnection";
+  };
+  age.secrets.bttf-lan = {
+    file = ../../secrets/bttf.lan.nmconnection.age;
+    path = "/etc/NetworkManager/system-connections/bttf.lan.nmconnection";
+  };
+  
+  ##### OpenVPN #####
+  age.secrets.ronaldOpenvpn = {
+    file = ../../secrets/ronald.openvpn.credentials.age;
   };
 
   home-manager.users.ronald = import ../../home/ronald/home.nix;
--- modules/crypto/secrets.nix
+++ modules/crypto/secrets.nix
@@ -18,18 +18,4 @@
   age.secrets.debbieNasCredentials = {
     file = ../../secrets/debbie.nas.credentials.age;
   };
-
-  age.secrets.ronaldOpenvpn = {
-    file = ../../secrets/ronald.openvpn.credentials.age;
-  };
-
-#  age.secrets.bttf-lan = {
-#   file = ../../secrets/bttf.lan.nmconnection.age;
-#   path = "/etc/NetworkManager/system-connections/bttf.lan.nmconnection";
-#  };
-#
-#  age.secrets.Ziggo-ap-4d4efe6 = {
-#   file = ../../secrets/Ziggo-ap-4d4efe6.nmconnection.age;
-#   path = "/etc/NetworkManager/system-connections/Ziggo-ap-4d4efe6.nmconnection";
-#  };
 }
