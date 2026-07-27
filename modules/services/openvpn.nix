d2c081fdbfe1c35f4cda15c9e8a110580520e59e
Ronald van Kouwen <Ronaldvk90@outlook.com>
2026-07-27 23:08:55 +0200

    Trial and error with OpenVPN

--- modules/services/openvpn.nix
+++ modules/services/openvpn.nix
@@ -4,7 +4,7 @@
     services.openvpn.servers = {
         thuis = {
             autoStart = false;
-            config = '' config config.age.secrets.ronaldOpenvpn.path '';
+            config = '' config /run/agenix/ronaldOpenvpn '';
             #config = config.age.secrets.ronaldOpenvpn.path;
             updateResolvConf = true;
         };
