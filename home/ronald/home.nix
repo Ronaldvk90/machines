7dffe7e6c624557b10551051ddfc05b27e7d7701
Ronald van Kouwen <Ronaldvk90@outlook.com>
2026-07-31 11:06:53 +0200

    Added Docker to codium plugins.

--- home/ronald/home.nix
+++ home/ronald/home.nix
@@ -71,10 +71,9 @@
 
   programs.vscodium = {
     enable = true;
-    #package = pkgs.vscodium;
-
     profiles.default.extensions = with pkgs.vscode-extensions; [
       jnoortheen.nix-ide
+      ms-azuretools.vscode-docker
     ];
   };
 
