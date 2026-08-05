68182f75e24d6f9b27fe95c99f7174659884aeb3
Ronald van Kouwen <Ronaldvk90@outlook.com>
2026-08-05 22:02:31 +0200

    Made container images PVE proof.

--- pvevms/jellyfin/configuration.nix
+++ pvevms/jellyfin/configuration.nix
@@ -3,6 +3,9 @@
 {
   boot.loader.systemd-boot.enable = false;
   boot.loader.grub.enable = false;
+  boot.isContainer = true;
+  systemd.services.systemd-modules-load.enable = false;
+  boot.kernelModules = [];
 
   home-manager.users.ronald = import ../../home/ronald/server.nix;
 
