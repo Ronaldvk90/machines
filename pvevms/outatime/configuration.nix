598370e02a48342db9588867866456cce7f3a1c9
Ronald van Kouwen <Ronaldvk90@outlook.com>
2026-08-05 21:46:50 +0200

    Proxmox PVE containers trial and error.

--- pvevms/outatime/configuration.nix
+++ pvevms/outatime/configuration.nix
@@ -4,6 +4,8 @@
   boot.loader.systemd-boot.enable = false;
   boot.loader.grub.enable = false;
   boot.isContainer = true;
+  systemd.services.systemd-modules-load.enable = false;
+  boot.kernelModules = [];
 
   home-manager.users.ronald = import ../../home/ronald/server.nix;
 
