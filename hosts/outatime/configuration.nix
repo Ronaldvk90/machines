8cd8030736ee4727381f6e36322e140c3a18cb58
Ronald van Kouwen <Ronaldvk90@outlook.com>
2026-08-06 14:15:04 +0200

    Removed systemD networking. Podman still not working?

--- hosts/outatime/configuration.nix
+++ hosts/outatime/configuration.nix
@@ -4,24 +4,39 @@
   home-manager.users.ronald = import ../../home/ronald/server.nix;
 
   networking.hostName = "outatime";
-  networking = {
-    dhcpcd.enable = false;
-    useDHCP = false;
-    useHostResolvConf = false;
-  };
 
-  systemd.network = {
-    enable = true;
-    networks."50-ens18" = {
-      matchConfig.Name = "ens18";
-      address = [ "10.10.10.11/24" ];
-      networkConfig = {
-        Gateway = "10.10.10.1";
-        DNS = [ "10.10.10.1" ];
-      };
-      linkConfig.RequiredForOnline = "routable";
+  networking = {
+    interfaces.ens18 = {
+      ipv4.addresses = [{
+        address = "10.10.10.11";
+        prefixLength = 24;
+      }];
+    };
+  
+    defaultGateway = {
+      address = "10.10.10.11";
+      interface = "ens18";
     };
   };
+
+  # networking = {
+  #   dhcpcd.enable = false;
+  #   useDHCP = false;
+  #   useHostResolvConf = false;
+  # };
+
+  # systemd.network = {
+  #   enable = true;
+  #   networks."50-ens18" = {
+  #     matchConfig.Name = "ens18";
+  #     address = [ "10.10.10.11/24" ];
+  #     networkConfig = {
+  #       Gateway = "10.10.10.1";
+  #       DNS = [ "10.10.10.1" ];
+  #     };
+  #     linkConfig.RequiredForOnline = "routable";
+  #   };
+  # };
   
   networking.enableIPv6 = false;
   networking.firewall.enable = false;
