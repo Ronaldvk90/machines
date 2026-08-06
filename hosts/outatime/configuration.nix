951d9f1b088c28e4c5194eede1d2900c2c66605c
Ronald van Kouwen <Ronaldvk90@outlook.com>
2026-08-06 14:22:28 +0200

    Ip trough networkmanager

--- hosts/outatime/configuration.nix
+++ hosts/outatime/configuration.nix
@@ -4,41 +4,11 @@
   home-manager.users.ronald = import ../../home/ronald/server.nix;
 
   networking.hostName = "outatime";
-
-  networking = {
-    interfaces.ens18 = {
-      ipv4.addresses = [{
-        address = "10.10.10.11";
-        prefixLength = 24;
-      }];
-    };
-  
-    defaultGateway = {
-      address = "10.10.10.11";
-      interface = "ens18";
-    };
-  };
-
-  # networking = {
-  #   dhcpcd.enable = false;
-  #   useDHCP = false;
-  #   useHostResolvConf = false;
-  # };
-
-  # systemd.network = {
-  #   enable = true;
-  #   networks."50-ens18" = {
-  #     matchConfig.Name = "ens18";
-  #     address = [ "10.10.10.11/24" ];
-  #     networkConfig = {
-  #       Gateway = "10.10.10.1";
-  #       DNS = [ "10.10.10.1" ];
-  #     };
-  #     linkConfig.RequiredForOnline = "routable";
-  #   };
-  # };
-  
-  networking.enableIPv6 = false;
+  networking.domain = "bttf.lan";
+  networking.networkmanager.enable = true;
+  networking.dhcpcd.enable = false;
+  networking.resolvconf.package = pkgs.openresolv;
+ 
   networking.firewall.enable = false;
   system.stateVersion = "26.05";
 }
