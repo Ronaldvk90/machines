514310ab3930d8268b09351071f2760f5bf3e2af
Ronald van Kouwen <Ronaldvk90@outlook.com>
2026-08-14 14:36:08 +0200

    disabled autostart wg0

--- modules/services/wireguardbiff.nix
+++ modules/services/wireguardbiff.nix
@@ -1,5 +1,6 @@
 { config, pkgs, lib, ... }:
 
 {
-networking.wg-quick.interfaces.wg0.configFile = "/run/agenix/wg0";
+  networking.wg-quick.interfaces.wg0.configFile = "/run/agenix/wg0";
+  systemd.services.wg-quick-wg0.wantedBy = lib.mkForce [ ];
 }
