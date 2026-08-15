1d4a6a378cf940df9ac22b4c0eba6af2d04ac1f0
Ronald van Kouwen <Ronaldvk90@outlook.com>
2026-08-15 14:22:39 +0200

    I had allow.unfree 2 times in different files.

--- modules/core/common.nix
+++ modules/core/common.nix
@@ -2,8 +2,7 @@
   age.identityPaths = [
     "/var/lib/agenix/agenix.key"
   ];
-
-  nixpkgs.config.allowUnfree = true;
+  
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
   users.mutableUsers = false;
